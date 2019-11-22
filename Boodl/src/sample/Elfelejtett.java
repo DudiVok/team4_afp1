576716422f063840fd90c4565da54317a0baf197
Donkó Márk <donkomark@gmail.com>
2019-11-22 10:19:34 +0100

    Regisztrációs felület és elfelejtett jelszó kész átjárás még nincs

---
+++ Boodl/src/sample/Elfelejtett.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Elfelejtett {
+    public void bt_Reg(ActionEvent event){
+        System.out.println("Regisztrációs panelen");
+    }
+    public void bt_Log(ActionEvent event){
+        System.out.println("Bejelentkezet panelen");
+    }
+    public void bt_For(ActionEvent event){
+        System.out.println("Elfelejtett panelen");
+    }
+}
---
+++ Boodl/src/sample/Reg.java
@@ -0,0 +1,12 @@
+package sample;
+
+import java.awt.event.ActionEvent;
+
+public class Reg {
+    public void bt_Reg(ActionEvent event){
+        System.out.println("Sikeres regisztráció");
+    }
+    public void bt_Log(ActionEvent event){
+        System.out.println("Vissza a bejelentkezésre");
+    }
+}
