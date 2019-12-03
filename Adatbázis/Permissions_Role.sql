fab4e84323fa371e0d983bd986b67f68c0a23896
Vajda Krisztián <uzleti02@gmail.com>
2019-12-03 09:30:25 +0100

    Modosítva

--- Adatbázis/Permissions_Role.sql
+++ Adatbázis/Permissions_Role.sql
@@ -3,4 +3,5 @@ Permission_Id INT,
 Role_Id INT
 )
 
-ALTER TABLE Permissions_Role ADD CONSTRAINT FK_PR FOREIGN KEY(Permission_Id) REFERENCES Permissions(Permissions_Id);
+ALTER TABLE Permissions_Role ADD CONSTRAINT FK_PP FOREIGN KEY(Permission_Id) REFERENCES Permissions(Permissions_Id);
+ALTER TABLE Permissions_Role ADD CONSTRAINT FK_PR FOREIGN KEY(Role_Id) REFERENCES Role_s(Role_Id);
