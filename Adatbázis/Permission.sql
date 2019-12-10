b3cce49786fbc73a06b210f93c84895b41ee6dd9
Imre Dávid <imre.david.levente@gmail.com>
2019-12-10 08:51:33 +0100

    Adatbázis struktúra hiba javítása

--- Adatbázis/Permission.sql
+++ Adatbázis/Permission.sql
@@ -1,6 +1,6 @@
 Create table Permissions(
 Pemissions_Id INT PRIMARY key,
-name VARCHAR2(250) NOT NULL
+name VARCHAR(250) NOT NULL
 )
 
 
--- Adatbázis/Roles.sql
+++ Adatbázis/Roles.sql
@@ -1,4 +1,4 @@
 Create table Role_s(
 Role_Id INT Primary Key,
-name VARCHAR2(250) NOT NULL
+name VARCHAR(250) NOT NULL
 )
