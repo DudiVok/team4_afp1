9e60924a73338485654c3ec54776af802bc6cd49
Vajda Krisztián <uzleti02@gmail.com>
2019-12-10 08:57:54 +0100

    hiba jav.

--- Adatbázis/Permissions_Role.sql
+++ Adatbázis/Permissions_Role.sql
@@ -1,7 +1,7 @@
-Create table Premissions_Role(
+Create table Permissions_Role(
 Permission_Id INT,
 Role_Id INT
-)
+);
 
 ALTER TABLE Permissions_Role ADD CONSTRAINT FK_PP FOREIGN KEY(Permission_Id) REFERENCES Permissions(Permissions_Id);
 ALTER TABLE Permissions_Role ADD CONSTRAINT FK_PR FOREIGN KEY(Role_Id) REFERENCES Role_s(Role_Id);
