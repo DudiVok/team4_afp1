9c1a2da017001521995cc1430c780ee9564c1de7
Vajda Krisztián <uzleti02@gmail.com>
2020-01-16 21:01:52 +0100

    Adatbázis connection, Tesztek

--- Boodl/poba.sql
+++ Boodl/poba.sql
@@ -1,10 +1,7 @@
-use schema BoodlDB;
+create or replace
+    definer = root@localhost procedure Insert_User(IN v_username varchar(250), IN v_password varchar(250),
+                                                   IN v_email varchar(250), IN v_role_id int)
+BEGIN
+	INSERT INTO users VALUES(NULL,v_username,v_password,v_email,v_role_id);
+END;
 
-SELECT * from genre;
-select * from instrument;
-select * from permissions;
-select * from role_s;
-select * from promo;
-select * from users;
-
-insert into genre (genre_id, genre_name) values (2,POP);
--- Boodl/src/sample/Reg.java
+++ Boodl/src/sample/Reg.java
@@ -3,17 +3,45 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
+import java.sql.*;
+import javax.sql.DataSource;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.ResultSet;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
 
+
 public class Reg {
     @FXML
+
     public AnchorPane rootPane;
+    public String url ="jdbc:mysql://localhost:3306/boodldb";
+    public String user="root";
+    public String password="";
+
+    public TextField Email;
+
+
+
     public void bt_Reg(ActionEvent event) throws IOException {
-        AnchorPane pane =FXMLLoader.load(getClass().getResource("Kezdo.fxml"));
+        try {
+            Connection myConn = DriverManager.getConnection(url,user,password);
+            Statement myStmt = myConn.createStatement();
+
+            String sql ="insert into users values ('NULL', 'Pista', 'email@email.hu', '1')";
+            myStmt.executeUpdate(sql);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        AnchorPane pane = (AnchorPane)FXMLLoader.load(this.getClass().getResource("Kezdo.fxml"));
+
         rootPane.getChildren().setAll(pane);
         System.out.println("Sikeres regisztráció");
     }
@@ -22,5 +50,4 @@ public class Reg {
         rootPane.getChildren().setAll(pane);
         System.out.println("Vissza a bejelentkezésre");
     }
-
 }
